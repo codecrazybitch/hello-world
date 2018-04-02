fa4b796455ba015367f9217d08602437a9435b41
codecrazybitch <gz_jia1413@163.com>
2018-04-02 11:08:04 +0800

    java多态与继承练习（修改）

--- noodle/src/MainDemo.java
+++ noodle/src/MainDemo.java
@@ -4,7 +4,10 @@ public class MainDemo {
         System.out.println("点单结果：" + "\n" + "种类：" +
                 noodle.getType() + "\n" + "数量：" + noodle.getNumbers() + "碗");
         System.out.println("请选择长面的长度：8,10,15,20,30");
+        System.out.println("请选择短面的长度：5，8，9，10");
+        ShortNoodle shotnoodle = new ShortNoodle("短面",1,20);
         LongNoodle longnoodle = new LongNoodle("长面", 1, 10);
+        shotnoodle.JugdeLength();
         longnoodle.JugdeLength();
         Person person = new Person();
         System.out.println("吃饭中........");
