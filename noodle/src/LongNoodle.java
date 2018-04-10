67a657231de293152bc338588d915d53dcfaf1e2
codecrazybitch <gz_jia1413@163.com>
2018-04-10 14:25:23 +0800

    Revert "java多态与继承练习（修改）"
    This reverts commit fa4b796455ba015367f9217d08602437a9435b41.

--- noodle/src/LongNoodle.java
+++ noodle/src/LongNoodle.java
@@ -9,11 +9,11 @@ public class LongNoodle extends Noodle{
     }
     public void JugdeLength(){
         if(getLength() >= 10 && getLength() <= 20){
-            System.out.println("这个长面的长度，吃着带劲");
+            System.out.println("这个面的长度，吃着带劲");
         }else if(getLength() < 10 && getLength() > 0){
-            System.out.println("这长面吃着没意思");
+            System.out.println("这面吃着没意思");
         }else{
-            System.out.println("没这种长面");
+            System.out.println("没这种面");
         }
     }
 }
